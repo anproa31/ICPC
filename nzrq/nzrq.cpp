f60ac4425392a4202d8bfe0224f6fbe3bfd0cbf6
ANBUIII\antoe <antoeta31@gmail.com>
2024-09-25 15:11:47 +0700

    nzqr

--- nzrq/nzrq.cpp
+++ nzrq/nzrq.cpp
@@ -1,1 +1,38 @@
-#include
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    freopen("../input.txt", "r", stdin);
+    freopen("../output.txt", "w", stdout);
+    
+    string num = "NZQR";
+    int t_minus[4] = {1, 1, 2, 3};
+    int t_div[4] = {2, 2, 2, 3};
+    char c1, c2, c3, ans;
+
+    cin >> c1 >> c2 >> c3;
+    int t1 = num.find(c1);
+    int t2 = num.find(c2);
+    int tm = max(t1, t2);
+    int t;
+
+    switch (c2)
+    {
+    case '+':
+        t = tm;
+        break;
+    case '*':
+        t = tm;
+        break;
+
+    case '-':
+        t = t_minus[tm];
+        break;
+
+    case '/':
+        t = t_div[tm];
+    }
+
+    cout << num[t];
+}
