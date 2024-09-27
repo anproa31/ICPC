dad6f75ee803856c946171b57bce782d84cd49a4
ANBUIII\antoe <antoeta31@gmail.com>
2024-09-28 00:48:07 +0700

    bai tap 1 decision table: keyboard

---
+++ keyboard/keyboard.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <unordered_map>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string s;
+    int n;
+    cin >> s >> n;
+
+    // Create a map to store the special keys and their respective "xi" values
+    unordered_map<char, int> key_map;
+    for (int i = 0; i < n; ++i)
+    {
+        char c;
+        int x;
+        cin >> x >> c;
+        key_map[c] = x;
+    }
+
+    unordered_map<char, int> char_count;
+    int total_key_presses = 0;
+    int count = 0;
+    for (int i = 0; i < s.length(); ++i)
+    {
+        if(key_map.find(s[i]) == key_map.end())
+        {
+            count++;
+        } else {
+            char_count[s[i]]++;
+        }
+    }
+
+    for (auto &it : char_count)
+    {
+        total_key_presses += ((it.second / key_map[it.first] + 1) + it.second) ;    
+    }
+
+    cout << count + total_key_presses<< endl;
+    return 0;
+}
