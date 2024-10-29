3e1ab2006354a412c67ec9896ccc2ab407f35002
ANBUIII\antoe <antoeta31@gmail.com>
2024-10-29 09:13:51 +0700

    update permutation

---
+++ Self-Learn/CSES/permutations.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+#define F first
+#define S second
+#define MAX 1e9 + 7
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    if(n <= 3 && n > 1) {
+        cout << "NO SOLUTION" << endl;
+    }
+    else {
+        for(int i = 2; i <= n; i += 2) {
+            cout << i << " ";
+        }
+        for(int i = 1; i <= n; i += 2) {
+            cout << i << " ";
+        }
+    }
+    return 0;
+}
