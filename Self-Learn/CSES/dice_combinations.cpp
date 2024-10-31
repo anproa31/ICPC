29491db5518ffe2ff7327df21668e1e379bf2c97
ANBUIII\antoe <antoeta31@gmail.com>
2024-10-31 15:29:55 +0700

    remove exe

---
+++ Self-Learn/CSES/dice_combinations.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+#define F first
+#define S second
+#define MOD 1000000007
+#define MAX 1000001
+
+int main()
+{
+    freopen("../input.txt", "r", stdin);
+    freopen("../output.txt", "w", stdout);
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    return 0;
+}
